caaffb2df3fb7599277ed65fde9b551e3338d2c8
Adam Russell <adamlr6+pub@gmail.com>
2012-10-05 13:08:03 -0500

    Correct misspelled word.

--- test/movie_rating_by_year_mapper_reducer_test.rb
+++ test/movie_rating_by_year_mapper_reducer_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/movie_data_source'
 
 
 class TestMovieRatingByYearMapperReducer < Test::Unit::TestCase
-  def test_finds_highst_rated_movie_for_1994
+  def test_finds_highest_rated_movie_for_1994
     dir = File.dirname(__FILE__)
     data_source = MovieDataSource.new File.join(dir, '..', 'movies.txt')
     mapper_reducer = MovieRatingByYearMapperReducer.new(data_source)
